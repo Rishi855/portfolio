5a2cca6e1aaa102b42ae7e50614b05bd735b89e6
rushi <rushikeshs855@gmail.com>
2023-03-14 23:18:06 +0530

    initial commit

---
+++ Portfoilo/js/theme.js
@@ -0,0 +1,13 @@
+function foo(){
+  window.open("mailto:xyz@abc.com");
+}
+function sendEmail() 
+{
+    window.location = "mailto:xyz@yourapplicationdomain.com";
+}
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".nav");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//   });
+// }); 
